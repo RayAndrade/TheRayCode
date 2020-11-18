b0608c0c04f47dd34653ea0e76a7346d7f01b009
TheRayCode <TheRayCode@RayAndrade.com>
2020-11-18 12:45:17 -0800

    touch up to php/composit-2

--- Java/Composite/src/TheRayCode/composite/example/shapes/CompoundShape.java
+++ Java/Composite/src/TheRayCode/composite/example/shapes/CompoundShape.java
@@ -6,26 +6,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CompoundShape extends BaseShape {
-    protected List<Shape> children = new ArrayList<>();
+    protected List<TheRayCode.composite.example.shapes.Shape> children = new ArrayList<>();
 
-    public CompoundShape(Shape... components) {
+    public CompoundShape(TheRayCode.composite.example.shapes.Shape... components) {
         super(0, 0, Color.BLACK);
         add(components);
     }
 
-    public void add(Shape component) {
+    public void add(TheRayCode.composite.example.shapes.Shape component) {
         children.add(component);
     }
 
-    public void add(Shape... components) {
+    public void add(TheRayCode.composite.example.shapes.Shape... components) {
         children.addAll(Arrays.asList(components));
     }
 
-    public void remove(Shape child) {
+    public void remove(TheRayCode.composite.example.shapes.Shape child) {
         children.remove(child);
     }
 
-    public void remove(Shape... components) {
+    public void remove(TheRayCode.composite.example.shapes.Shape... components) {
         children.removeAll(Arrays.asList(components));
     }
 
@@ -39,7 +39,7 @@ public class CompoundShape extends BaseShape {
             return 0;
         }
         int x = children.get(0).getX();
-        for (Shape child : children) {
+        for (TheRayCode.composite.example.shapes.Shape child : children) {
             if (child.getX() < x) {
                 x = child.getX();
             }
@@ -53,7 +53,7 @@ public class CompoundShape extends BaseShape {
             return 0;
         }
         int y = children.get(0).getY();
-        for (Shape child : children) {
+        for (TheRayCode.composite.example.shapes.Shape child : children) {
             if (child.getY() < y) {
                 y = child.getY();
             }
@@ -65,7 +65,7 @@ public class CompoundShape extends BaseShape {
     public int getWidth() {
         int maxWidth = 0;
         int x = getX();
-        for (Shape child : children) {
+        for (TheRayCode.composite.example.shapes.Shape child : children) {
             int childsRelativeX = child.getX() - x;
             int childWidth = childsRelativeX + child.getWidth();
             if (childWidth > maxWidth) {
@@ -79,7 +79,7 @@ public class CompoundShape extends BaseShape {
     public int getHeight() {
         int maxHeight = 0;
         int y = getY();
-        for (Shape child : children) {
+        for (TheRayCode.composite.example.shapes.Shape child : children) {
             int childsRelativeY = child.getY() - y;
             int childHeight = childsRelativeY + child.getHeight();
             if (childHeight > maxHeight) {
@@ -91,14 +91,14 @@ public class CompoundShape extends BaseShape {
 
     @Override
     public void move(int x, int y) {
-        for (Shape child : children) {
+        for (TheRayCode.composite.example.shapes.Shape child : children) {
             child.move(x, y);
         }
     }
 
     @Override
     public boolean isInsideBounds(int x, int y) {
-        for (Shape child : children) {
+        for (TheRayCode.composite.example.shapes.Shape child : children) {
             if (child.isInsideBounds(x, y)) {
                 return true;
             }
@@ -109,7 +109,7 @@ public class CompoundShape extends BaseShape {
     @Override
     public void unSelect() {
         super.unSelect();
-        for (Shape child : children) {
+        for (TheRayCode.composite.example.shapes.Shape child : children) {
             child.unSelect();
         }
     }
