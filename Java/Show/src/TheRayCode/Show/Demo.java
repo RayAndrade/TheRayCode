94c03308cc2cf3ff772bf50487ba56b148f255f3
TheRayCode <TheRayCode@RayAndrade.com>
2020-11-21 18:40:18 -0800

    prep for Decorator demo-1

--- Java/Show/src/TheRayCode/Show/Demo.java
+++ Java/Show/src/TheRayCode/Show/Demo.java
@@ -1,11 +1,10 @@
-package TheRayCode.composite;
+package TheRayCode.Show;
 
-import TheRayCode.composite.editor.ImageEditor;
-import TheRayCode.composite.shapes.Circle;
-import TheRayCode.composite.shapes.CompoundShape;
-import TheRayCode.composite.shapes.Dot;
-import TheRayCode.composite.shapes.Rectangle;
-
+import TheRayCode.Show.editor.ImageEditor;
+import TheRayCode.Show.shapes.Circle;
+import TheRayCode.Show.shapes.CompoundShape;
+import TheRayCode.Show.shapes.Dot;
+import TheRayCode.Show.shapes.Rectangle;
 import java.awt.*;
 
 public class Demo {
@@ -14,21 +13,21 @@ public class Demo {
         ImageEditor editor = new ImageEditor();
 
         editor.loadShapes(
-                new Circle(10, 10, 10, Color.BLUE),
+                new Circle(10, 10, Color.BLUE),
 
                 new CompoundShape(
-                        new Circle(110, 110, 50, Color.RED),
+                        new Circle(110, 110, Color.RED),
                         new Dot(160, 160, Color.RED)
                 ),
 
                 new CompoundShape(
-                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Rectangle(250, 250, Color.GREEN),
                         new Dot(240, 240, Color.GREEN),
                         new Dot(240, 360, Color.GREEN),
                         new Dot(360, 360, Color.GREEN),
                         new Dot(360, 240, Color.GREEN)
                 )
         );
+
     }
 }
-
