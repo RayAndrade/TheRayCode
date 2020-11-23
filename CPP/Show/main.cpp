c601671e719eb1a12c699dd0d7ef3bb855722948
Ray Andrade <TheRayCode@RayAndrade.com>
2020-11-22 18:10:20 -0800

    using gui

--- CPP/Show/main.cpp
+++ CPP/Show/main.cpp
@@ -1,36 +1,8 @@
-#include "ConcreteComponent.h"
-#include "ConcreteDecoratorA.h"
-#include "ConcreteDecoratorB.h"
-
-void ClientCode(Component* component) {
-    // ...
-    std::cout << "RESULT: " << component->Operation();
-    // ...
-}
+#include <iostream>
 
 int main() {
-    /**
-     * This way the client code can support both simple components...
-     */
-    Component* simple = new ConcreteComponent;
-    std::cout << "Client: I've got a simple component:\n";
-    ClientCode(simple);
-    std::cout << "\n\n";
-    /**
-     * ...as well as decorated ones.
-     *
-     * Note how decorators can wrap not only simple components but the other
-     * decorators as well.
-     */
-    Component* decorator1 = new ConcreteDecoratorA(simple);
-    Component* decorator2 = new ConcreteDecoratorB(decorator1);
-    std::cout << "Client: Now I've got a decorated component:\n";
-    ClientCode(decorator2);
+
     std::cout << "\n";
-
-    delete simple;
-    delete decorator1;
-    delete decorator2;
-
+    std::cout << "The Ray Code is AWESOME!!!\n";
     return 0;
 }
