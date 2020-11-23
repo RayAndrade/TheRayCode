d6f0ef8bd66f4a49dfae790f5f7a20fdc1272606
Ray Andrade <TheRayCode@RayAndrade.com>
2020-11-23 00:00:43 -0800

    uml for decorator

--- CPP/Show/main.cpp
+++ CPP/Show/main.cpp
@@ -1,6 +1,29 @@
-#include <iostream>
+#include "ConcreteComponent.h"
+#include "ConcreteDecoratorA.h"
+#include "ConcreteDecoratorB.h"
+
+void ClientCode(Component* component) {
+    // ...
+    std::cout << "RESULT: " << component->Operation();
+    // ...
+}
 
 int main() {
+
+    Component* simple = new ConcreteComponent;
+    std::cout << "Client: I've got a simple component:\n";
+    ClientCode(simple);
+    std::cout << "\n\n";
+
+    Component* decorator1 = new ConcreteDecoratorA(simple);
+    Component* decorator2 = new ConcreteDecoratorB(decorator1);
+    std::cout << "Client: Now I've got a decorated component:\n";
+    ClientCode(decorator2);
+    std::cout << "\n";
+
+    delete simple;
+    delete decorator1;
+    delete decorator2;
 
 
     std::cout << "\n";
