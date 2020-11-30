6e5f9091c33ca161395862072c1a7dd0d77d27b2
Ray Andrade <TheRayCode@RayAndrade.com>
2020-11-30 14:48:12 -0800

    Started Facade Java Demo 2

---
+++ Java/Show/src/TheRayCode/Show/some_complex_media_library/Codec.java
@@ -0,0 +1,4 @@
+package TheRayCode.Show.some_complex_media_library;
+
+public interface Codec {
+}
---
+++ Java/Show/src/TheRayCode/Show/some_complex_media_library/MPEG4CompressionCodec.java
@@ -0,0 +1,5 @@
+package TheRayCode.Show.some_complex_media_library;
+
+public class MPEG4CompressionCodec implements Codec {
+    public String type = "mp4";
+}
