16ad5905ebdeba275880550fceffbcb27d830175
Ray Andrade <TheRayCode@RayAndrade.com>
2021-02-15 15:45:46 -0800

    CSharp Command 02-15-21

--- Csharp/Show/ChainOfResponsibility/IHandler.cs
+++
@@ -1,9 +0,0 @@
-namespace ChainOfResponsibility
-{
-    public interface IHandler
-    {
-        IHandler SetNext(IHandler handler);
-        
-        object Handle(object request);
-    }
-}
--- Csharp/Show/ChainOfResponsibility/Program.cs
+++ Csharp/Show/ChainOfResponsibility/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+//using System.Collections.Generic;
 
 namespace ChainOfResponsibility
 {
@@ -7,21 +7,7 @@ namespace ChainOfResponsibility
     {
         static void Main(string[] args)
         {
-            // The other part of the client code constructs the actual chain.
-            var monkey = new MonkeyHandler();
-            var squirrel = new SquirrelHandler();
-            var dog = new DogHandler();
-
-            monkey.SetNext(squirrel).SetNext(dog);
-
-            // The client should be able to send a request to any handler, not
-            // just the first one in the chain.
-            Console.WriteLine("Chain: Monkey > Squirrel > Dog\n");
-            Client.ClientCode(monkey);
-            Console.WriteLine();
-
-            Console.WriteLine("Subchain: Squirrel > Dog\n");
-            Client.ClientCode(squirrel);
+            Console.WriteLine("The Ray Code is AWESOME!!!");
         }
     }
 }
