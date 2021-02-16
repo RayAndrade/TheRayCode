5a525e66d50b38ba850f3723143695424e3a39c6
Ray Andrade <TheRayCode@RayAndrade.com>
2021-02-15 19:28:42 -0800

    CSharp Command 02-15-21

--- Csharp/Command/Command/Program.cs
+++ Csharp/Command/Command/Program.cs
@@ -7,13 +7,7 @@ namespace Command
     {
         static void Main(string[] args)
         {
-            // The client code can parameterize an invoker with any commands.
-            Invoker invoker = new Invoker();
-            invoker.SetOnStart(new SimpleCommand("Say Hi!"));
-            Receiver receiver = new Receiver();
-            invoker.SetOnFinish(new ComplexCommand(receiver, "Send email", "Save report"));
-
-            invoker.DoSomethingImportant();
+            
         }
     }
 }
