c68ebd3961a8e0015b89d62db4fbd8c76003ebe0
Ray Andrade <TheRayCode@RayAndrade.com>
2021-02-15 19:42:16 -0800

    CSharp Command 02-15-21

--- Csharp/Command/Command/Program.cs
+++ Csharp/Command/Command/Program.cs
@@ -7,7 +7,15 @@ namespace Command
     {
         static void Main(string[] args)
         {
+            // The client code can parameterize an invoker with any commands.
+            Invoker invoker = new Invoker();
+            invoker.SetOnStart(new SimpleCmd("Say Hi!"));
+            Receiver receiver = new Receiver();
+            invoker.SetOnFinish(new ComplexCmd(receiver, "Send email", "Save report"));
+
+            invoker.DoSomethingImportant();
             
+            Console.WriteLine("The Ray Code is AWESOME!!!");
         }
     }
 }
