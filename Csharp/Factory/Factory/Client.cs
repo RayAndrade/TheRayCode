b4ba6a599c8d341e41d12ea0bffe743aea843ca8
Ray Andrade <TheRayCode@RayAndrade.com>
2021-02-25 18:28:32 -0800

    C# Factory delete

--- Csharp/Factory/Factory/Client.cs
+++ Csharp/Factory/Factory/Client.cs
@@ -7,12 +7,12 @@ namespace TheRayCode.Factory.DesignPatterns
         public void Main()
         {
             Console.WriteLine("App: Launched with the ConcreteCreator1.");
-            ClientCode(new ConcreteCreator1());
+            ClientCode(new Creator1());
             
             Console.WriteLine("");
 
             Console.WriteLine("App: Launched with the ConcreteCreator2.");
-            ClientCode(new ConcreteCreator2());
+            ClientCode(new Creator2());
         }
         public void ClientCode(Creator creator)
         {
--- Csharp/Factory/Factory/IBuilder.cs
+++ Csharp/Factory/Factory/IBuilder.cs
@@ -8,5 +8,4 @@ namespace TheRayCode.Factory.DesignPatterns
         
         void BuildPartC();
     }
-
 }
