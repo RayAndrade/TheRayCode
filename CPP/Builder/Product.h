6d983290d5bcaf26e1838b7f595522d91063c346
Ray Andrade <TheRayCode@RayAndrade.com>
2021-03-24 20:36:40 -0700

    Builder source c++

---
+++ CPP/Builder/Product.h
@@ -0,0 +1,31 @@
+//
+// Created by ray on 8/13/20.
+//
+
+#ifndef BUILDER_PRODUCT_H
+#define BUILDER_PRODUCT_H
+#include <iostream>
+#include <vector>
+
+
+
+class Product {
+
+public:
+    std::vector<std::string> parts_;
+    void ListParts()const{
+        std::cout << "Product parts: ";
+        for (size_t i=0;i<parts_.size();i++){
+            if(parts_[i]== parts_.back()){
+                std::cout << parts_[i];
+            }else{
+                std::cout << parts_[i] << ", ";
+            }
+        }
+        std::cout << "\n\n";
+    }
+
+};
+
+
+#endif //BUILDER_PRODUCT_H
---
+++ CPP/Builder/SolidBuilder.h
@@ -0,0 +1,43 @@
+//
+// Created by ray on 8/13/20.
+//
+
+#ifndef BUILDER_SOLIDBUILDER_H
+#define BUILDER_SOLIDBUILDER_H
+
+#include "Builder.h"
+#include "Product.h"
+
+
+class SolidBuilder: public Builder {
+private:
+    Product* product;
+public:
+    SolidBuilder(){
+        this->Reset();
+    }
+    ~SolidBuilder(){
+        delete product;
+    }
+    void Reset(){
+        this->product= new Product();
+    }
+    void ProducePartA()const override{
+        this->product->parts_.push_back("PartA");
+    }
+    void ProducePartB()const override{
+        this->product->parts_.push_back("PartB");
+    }
+    void ProducePartC()const override{
+        this->product->parts_.push_back("PartC");
+    }
+    Product* GetProduct() {
+        Product* result= this->product;
+        this->Reset();
+        return result;
+    }
+
+};
+
+
+#endif //BUILDER_SOLIDBUILDER_H
