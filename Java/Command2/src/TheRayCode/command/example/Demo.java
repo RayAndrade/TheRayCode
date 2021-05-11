f039cf3490bbc2146ddc9c3c8d4ec660e3603c45
Ray Andrade <TheRayCode@RayAndrade.com>
2021-05-10 22:29:37 -0700

    Java/Command2

---
+++ Java/Command2/src/TheRayCode/command/example/Demo.java
@@ -0,0 +1,10 @@
+package TheRayCode.command.example;
+
+import TheRayCode.command.example.editor.Editor;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
---
+++ Java/Command2/src/TheRayCode/command/example/commands/CopyCommand.java
@@ -0,0 +1,18 @@
+package TheRayCode.command.example.commands;
+
+import TheRayCode.command.example.editor.Editor;
+
+public class CopyCommand extends Command {
+
+    public CopyCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+}
+
+
