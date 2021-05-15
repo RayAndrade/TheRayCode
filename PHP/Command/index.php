29079c79141918383098cca085446ad44203f047
Ray Andrade <TheRayCode@RayAndrade.com>
2021-05-15 14:10:24 -0700

    PHP/Command/index.php

--- PHP/Command/index.php
+++ PHP/Command/index.php
@@ -9,8 +9,8 @@ include_once ('Invoker.php');
  * The client code can parameterize an invoker with any commands.
  */
 $invoker = new Invoker;
-$invoker->setOnStart(new SimpleCommand("Say Hi!"));
+$invoker->setOnStart(new SimpleCommand("Do Some Command"));
 $receiver = new Receiver;
-$invoker->setOnFinish(new ComplexCommand($receiver, "Send email", "Save report"));
+$invoker->setOnFinish(new ComplexCommand($receiver, "Do one request", "Do the next request"));
 
 $invoker->doSomethingImportant();
