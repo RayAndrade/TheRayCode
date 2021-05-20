45ade83fafb9f6c39250031616db01ffd03655fd
Ray Andrade <TheRayCode@RayAndrade.com>
2021-05-20 11:09:17 -0700

    Csharp/Iterator/

--- Java/Iterator/src/TheRayCode/iterator/example/Demo.java
+++ Java/Iterator/src/TheRayCode/iterator/example/Demo.java
@@ -33,20 +33,20 @@ public class Demo {
         }
 
         SocialSpammer spammer = new SocialSpammer(network);
-        spammer.sendSpamToFriends("anna.smith@bing.com",
-                "Hey! This is Anna's friend Josh. Can you do me a favor and like this post [link]?");
-        spammer.sendSpamToCoworkers("anna.smith@bing.com",
-                "Hey! This is Anna's boss Jason. Anna told me you would be interested in [link].");
+        spammer.sendSpamToFriends("TheRayCode@RayAndrade.com",
+                "Hey! This is Ray's friend Junk. Can you do me a favor and like this post TheRayCode.org?");
+        spammer.sendSpamToCoworkers("TheRayCode@RayAndrade.com",
+                "Hey! This is Ray's boss Support. Ray told me you would be interested in TheRayCode.org.");
     }
 
     public static List<Profile> createTestProfiles() {
         List<Profile> data = new ArrayList<Profile>();
-        data.add(new Profile("anna.smith@bing.com", "Anna Smith", "friends:mad_max@ya.com", "friends:catwoman@yahoo.com", "coworkers:sam@amazon.com"));
-        data.add(new Profile("mad_max@ya.com", "Maximilian", "friends:anna.smith@bing.com", "coworkers:sam@amazon.com"));
+        data.add(new Profile("TheRayCode@RayAndrade.com", "Ray1 Smith", "friends:mad_max@ya.com", "friends:catwoman@yahoo.com", "coworkers:sam@amazon.com"));
+        data.add(new Profile("mad_max@ya.com", "Maximilian", "friends:TheRayCode@RayAndrade.com", "coworkers:sam@amazon.com"));
         data.add(new Profile("bill@microsoft.eu", "Billie", "coworkers:avanger@ukr.net"));
         data.add(new Profile("avanger@ukr.net", "John Day", "coworkers:bill@microsoft.eu"));
-        data.add(new Profile("sam@amazon.com", "Sam Kitting", "coworkers:anna.smith@bing.com", "coworkers:mad_max@ya.com", "friends:catwoman@yahoo.com"));
-        data.add(new Profile("catwoman@yahoo.com", "Liza", "friends:anna.smith@bing.com", "friends:sam@amazon.com"));
+        data.add(new Profile("sam@amazon.com", "Sam Kitting", "coworkers:TheRayCode@RayAndrade.com", "coworkers:mad_max@ya.com", "friends:catwoman@yahoo.com"));
+        data.add(new Profile("catwoman@yahoo.com", "Liza", "friends:TheRayCode@RayAndrade.com", "friends:sam@amazon.com"));
         return data;
     }
 }
