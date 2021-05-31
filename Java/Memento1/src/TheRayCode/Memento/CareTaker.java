609f55fed6cb66cfd94e0b2b7c6671adcfbccdf1
Ray Andrade <TheRayCode@RayAndrade.com>
2021-05-31 09:52:28 -0700

    CPP/Memento2/*

--- Java/Memento1/src/TheRayCode/Memento/CareTaker.java
+++ Java/Memento1/src/TheRayCode/Memento/CareTaker.java
@@ -5,9 +5,10 @@ import java.util.List;
 
 public class CareTaker {
     private List<Memento> mementoList = new ArrayList<Memento>();
-    public void add(Memento state){
+    public void add(Memento state) {
         mementoList.add(state);
     }
+
     public Memento get(int index){
         return mementoList.get(index);
     }
---
+++ Java/Memento2/src/TheRayCode/Memento/Caretaker.java
@@ -0,0 +1,19 @@
+package TheRayCode.Memento;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Caretaker {
+
+    private List<Memento> ledTvList = new ArrayList<Memento>();
+
+    public void addMemento( Memento m )
+    {
+        ledTvList.add(m);
+        System.out.println("LED TV's snapshots Maintained by CareTaker :" + ledTvList);
+    }
+
+    public Memento getMemento( int index )
+    {
+        return ledTvList.get(index);
+    }
+}
