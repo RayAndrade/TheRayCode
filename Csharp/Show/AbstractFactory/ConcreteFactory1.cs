3df03c0ff6a86b71b0ba82994d095c24ebbe4fc8
Ray Andrade <TheRayCode@RayAndrade.com>
2021-08-26 13:06:24 -0700

    Csharp/AbstractFactory/Show

---
+++ Csharp/Show/AbstractFactory/ConcreteFactory1.cs
@@ -0,0 +1,14 @@
+namespace AbstractFactory
+{
+    public class ConcreteFactory1: AbstractFactory
+    {
+        public ProductA CreateProductA()
+        {
+            return new SolidProductA1();
+        }
+        public ProductB CreateProductB()
+        {
+            return new SolidProductB1();
+        }
+    }
+}
---
+++ Csharp/Show/AbstractFactory/SolidProductA2.cs
@@ -0,0 +1,10 @@
+namespace AbstractFactory
+{
+    public class SolidProductA2: ProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "The Ray Code is AWESOME!!!";
+        }
+    }
+}
