f4b7a8476b11676633fe8d4651039ac1b2261377
Ray Andrade <TheRayCode@RayAndrade.com>
2021-12-08 12:55:20 -0800

    CPP/SHOW/Builder

---
+++ CPP/Show/Builder.h
@@ -0,0 +1,14 @@
+//
+// Created by ray on 12/8/21.
+//
+
+#ifndef SHOW_BUILDER_H
+#define SHOW_BUILDER_H
+class Builder{
+public:
+    virtual ~Builder(){}
+    virtual void ProducePartA() const =0;
+    virtual void ProducePartB() const =0;
+    virtual void ProducePartC() const =0;
+};
+#endif //SHOW_BUILDER_H
