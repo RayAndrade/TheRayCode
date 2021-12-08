8aca3775cf696d4448de7c83958dbb5e5d2943e4
Ray Andrade <TheRayCode@RayAndrade.com>
2021-12-07 18:25:18 -0800

    CPP/SHOW/Builder

---
+++ CPP/Show/Director.h
@@ -0,0 +1,27 @@
+//
+// Created by ray on 12/7/21.
+//
+
+#ifndef SHOW_DIRECTOR_H
+#define SHOW_DIRECTOR_H
+
+#include "Builder.h"
+
+class Director{
+private:
+    Builder* builder;
+public:
+    void set_builder(Builder* builder){
+        this->builder=builder;
+    }
+    void BuildMinimalViableProduct(){
+        this->builder->ProducePartA();
+    }
+    void BuildFullFeaturedProduct(){
+        this->builder->ProducePartA();
+        this->builder->ProducePartB();
+        this->builder->ProducePartC();
+    }
+
+};
+#endif //SHOW_DIRECTOR_H
