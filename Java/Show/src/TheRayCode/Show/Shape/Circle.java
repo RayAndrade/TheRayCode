e6742a2f15645c24aa915e52ff515a5f11a44cd8
Ray Andrade <TheRayCode@RayAndrade.com>
2022-01-26 14:15:27 -0800

    Java/Show

--- Java/Show/src/TheRayCode/Show/Shape/Circle.java
+++ Java/Show/src/TheRayCode/Show/Shape/Circle.java
@@ -1,2 +1,23 @@
-package TheRayCode.Show.Shape;public class Circle {
+package TheRayCode.Show.Shape;
+
+public class Circle extends Shape{
+    public int radius;
+    public Circle() {
+    }
+    public Circle(Circle target) {
+        super(target);
+        if (target != null) {
+            this.radius = target.radius;
+        }
+    }
+    @Override
+    public Shape clone() {
+        return new Circle(this);
+    }
+    @Override
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Circle) || !super.equals(object2)) return false;
+        Circle shape2 = (Circle) object2;
+        return shape2.radius == radius;
+    }
 }
