c6f7b1af17fb875f444079c71e793726e5945b5e
Ray Andrade <TheRayCode@RayAndrade.com>
2022-02-16 01:46:17 -0800

    Java/Factory2

--- Java/Show/src/TheRayCode/Show/Factory2/Demo.java
+++ Java/Show/src/TheRayCode/Show/Factory2/Demo.java
@@ -1,4 +1,4 @@
-package TheRayCode.Show.Factory;
+package TheRayCode.Show.Factory2;
 
 public class Demo {
     public static void main(String[] args) {
---
+++ Java/Show/src/TheRayCode/Show/Factory2/UFOEnemyShip.java
@@ -0,0 +1,13 @@
+package TheRayCode.Show.Factory2;
+
+public class UFOEnemyShip extends EnemyShip  {
+
+    public UFOEnemyShip(){
+
+        setName("UFO Enemy Ship");
+
+        setDamage(20.0);
+
+    }
+
+}
