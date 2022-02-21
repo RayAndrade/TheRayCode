f3db3e242cb4eeaaeaadf99ef42f2512e9a49cdc
Ray Andrade <TheRayCode@RayAndrade.com>
2022-02-20 19:52:53 -0800

    PHP/Factory4

---
+++ PHP/Factory4/factory.php
@@ -0,0 +1,40 @@
+<?php
+class AnimalsFactory{
+    /**
+     * @throws Exception
+     */
+    public static function  getAnimal($str)
+    {
+        if ($str == "cat") {
+            return new Cat();
+        } else if ($str == "tiger") {
+            $ob = new Cat();
+            $ob->setCatType("tiger");
+            return $ob;
+        } else if ($str == "dog") {
+            return new Dog();
+        }
+        throw new Exception('animal type does not exist');
+
+    }
+}
+
+class Cat
+{
+    private $catType;
+    function __toString()
+    {
+        return "cat" . $this->catType;
+    }
+    function setCatType($str)
+    {
+        $this->catType = $str;
+    }
+}
+class Dog
+{
+    function __toString()
+    {
+        return "dog";
+    }
+}
---
+++ PHP/Factory4/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * https://www.youtube.com/watch?v=tfpZqhaM5kU
+ * The Factory Design Pattern in PHP
+
+ * life michael
+ *
+ */
+
+include ('factory.php');
+$obA = AnimalsFactory::getAnimal("dog");
+$obB = AnimalsFactory::getAnimal("tiger");
+echo $obA;
+echo "<p>";
+echo $obB;
+?>
