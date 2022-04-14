4b92c801bb9e6ed1f546f3c4dc0f7bfe34628df1
Ray Andrade <TheRayCode@RayAndrade.com>
2022-04-14 11:45:03 -0700

    Java/Factory 1

--- Java/Factory/show/src/systems/Android.java
+++
@@ -1,7 +0,0 @@
-package systems;
-
-public class Android implements OS{
-    public void spec() {
-        System.out.println("The Ray Code supports Android");
-    }
-}
--- Java/Factory/show/src/systems/Windows.java
+++
@@ -1,7 +0,0 @@
-package systems;
-
-public class Windows  implements OS{
-    public void spec() {
-        System.out.println("Windows is proprietary software");
-    }
-}
