234fb51871e5e643f3b5b942d4e6c558b34e7579
Ray Andrade <TheRayCode@RayAndrade.com>
2022-06-06 11:20:25 -0700

    CSharp/Factory

---
+++ Csharp/Factroy/F2/Factory/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TheRayCode.Factory.DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello world");
+            new Client().Main();
+        }
+    }
+}
