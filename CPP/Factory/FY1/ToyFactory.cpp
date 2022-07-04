d1901b01b5d46ed0ba3caba2e3e670c37a54a8b3
Ray Andrade <TheRayCode@RayAndrade.com>
2022-07-04 12:44:31 -0700

    CPP/Factory/FY1

---
+++ CPP/Factory/FY1/ToyFactory.cpp
@@ -0,0 +1,37 @@
+//
+// Created by ray on 7/4/22.
+//
+#include <iostream>
+using namespace std;
+
+#include "Object.cpp"
+
+class ToyFactory{
+public:
+    static Toy * createToy(int type){
+        Toy *toy = NULL;
+
+        switch(type){
+            case 1:{
+                toy = new Car;
+                break;
+            }
+            case 2:{
+                toy = new Bike;
+            }
+            case 3:{
+                toy = new Plane;
+                break;
+            }
+            default:{
+                cout << "Invalid toy type please re-enter type" << endl;
+                return NULL;
+            }
+        }
+        toy->prepareParts();
+        toy->combineParts();
+        toy->assembleParts();
+        toy->applyLabel();
+        return toy;
+    }
+};
---
+++ CPP/Factory/FY1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// https://www.youtube.com/watch?v=XyNWEWUSa5E
+// Factory Design Pattern in C++
+// CppNuts
+
+#include "ToyFactory.cpp"
+
+int main() {
+// client code starts
+    int type;
+    while(1){
+        cout << "Enter type of Zero for exit" << endl;
+        cin >> type;
+        if(!type)
+            break;
+        Toy *v = ToyFactory::createToy(type);
+        if(v){
+            v->showProduct();
+            delete v;
+        }
+    }
+    cout << "Exit...";
+//Client code ends
+    return 0;
+}
