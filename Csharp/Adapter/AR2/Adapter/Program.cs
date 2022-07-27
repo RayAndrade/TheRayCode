62520a826f305c09b7d5872b57b908cb93d31253
Ray Andrade <TheRayCode@RayAndrade.com>
2022-07-27 02:08:01 -0700

    Charp/Adapter/AR2

---
+++ Csharp/Adapter/AR2/Adapter/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//  https://youtu.be/u3IE_ENPUeg 
+
+
+using System;
+using System.Collections.Generic;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IClientAdapter iClientAdapter = new ClientAdapter();
+            Client objClient = new Client(iClientAdapter);
+            List<string> listOfString = objClient.GetListOfClientItem();
+            foreach (var item in listOfString)
+            {
+                Console.WriteLine(item);
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
