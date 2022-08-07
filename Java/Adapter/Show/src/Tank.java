3f509aafe915e3154ab2607c664ed6fe7800ccec
Ray Andrade <TheRayCode@RayAndrade.com>
2022-08-06 21:05:04 -0700

    Java/Adapter/AR2

---
+++ Java/Adapter/Show/src/Tank.java
@@ -0,0 +1,16 @@
+public class Tank implements Attacker {
+    @Override
+    public void fireWeapon() {
+
+    }
+
+    @Override
+    public void driveForward() {
+
+    }
+
+    @Override
+    public void assignDriver(String driverName) {
+
+    }
+}
