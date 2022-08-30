28f2bb1f91542fbd96899cbe9f2a52140f7e920b
Ray Andrade <TheRayCode@RayAndrade.com>
2022-08-29 21:19:51 -0700

    Adapter

---
+++ Csharp/Adapter/AR2/Adapter/Adapter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Adapter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ICustomer customer = new CustomerAdapter();
+            IEnumerable<CustomerDTO> data = customer.GetCustomers();
+            Console.WriteLine(data);
+        }
+    }
+}
