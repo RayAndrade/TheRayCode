c927c1abcf1f1d8fe6d09efaf85adc6651e86e6f
Ray Andrade <TheRayCode@RayAndrade.com>
2022-10-11 09:32:07 -0700

    Java/Creational/Factory

--- Java/Creational/Factory/FY01/src/FactoryMain.java
+++ Java/Creational/Factory/FY01/src/FactoryMain.java
@@ -1,7 +1,39 @@
 import com.phone.OS;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class FactoryMain {
     public static void main(String[] args) {
+
+
+        BufferedReader scan = new BufferedReader(new InputStream() {
+            @Override
+            public int read() throws IOException {
+                return 0;
+            }
+        });
+
+
+        int expression = int can1;
+
+        while (contine) {
+            if (expression == 1){
+
+            }
+            if (expression == 2){
+
+            }
+            if (expression == 3){
+
+            }
+            if (expression == 0){
+                break;
+            }
+        }
+
+
 
         OperatingSystemFactory osf = new OperatingSystemFactory();
 
