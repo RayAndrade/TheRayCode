1f18adb64770d13a99f22ff2c3f6e97cc0ea6978
Ray Andrade <TheRayCode@RayAndrade.com>
2022-11-24 17:01:26 -0800

    *

--- CPP/Creational/Factory/Show/main.cpp
+++ CPP/Creational/Factory/Show/main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include "ToyFactory.h"
 
 int main() {
-    std::cout << "The Ray Code is AWESOME!!!";
+// client code starts
+
+    int type;
+    while(1){
+        std::cout << "1 Car, 2 Bike, 3 Plane Enter type or Zero for exit" << std::endl;
+        std::cin >> type;
+        if(!type)
+            break;
+        Toy *v = ToyFactory::createToy(type);
+        if(v){
+            v->showProduct();
+            delete v;
+        }
+    }
+    std::cout << "Exit...";
+
+
+//Client code ends
     return 0;
 }
