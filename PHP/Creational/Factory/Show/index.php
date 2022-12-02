db14afec751889ea5e8a9d9f5f6718a1ad61a289
Ray Andrade <TheRayCode@RayAndrade.com>
2022-12-02 01:08:12 -0800

    *

--- PHP/Creational/Factory/Show/index.php
+++ PHP/Creational/Factory/Show/index.php
@@ -5,21 +5,5 @@
 
 namespace TheRayCode\Factory;
 
-include_once ('ShapeFactory.php');
+echo "The Ray Code is <b>AWESOME!!!</b><br/>";
 
-function drawStuff(Shape $shape){
-    $shape ->draw();
-}
-
-$shape1 = new Rectangle(new Position());
-$shape2 = new Rectangle(new Position());
-
-drawStuff($shape1);
-drawStuff($shape2);
-
-$factory = new ShapeFactory("Rectangle");
-$rect = $factory->create("Rectangle");
-echo $rect->draw();
-
-
-//echo "The Ray Code is AWESOME!!!<br/>";$rect ->draw();
--- PHP/Creational/Factory/Show/script/page01.md
+++ PHP/Creational/Factory/Show/script/page01.md
@@ -1,2 +1,13 @@
 
+// https://www.youtube.com/watch?v=VuBVAgwMfLE
+
+```php
+
+namespace TheRayCode\Factory;
+
+echo "The Ray Code is <b>AWESOME!!!</b><br/>";
+
+```
+
+
 [page 2](./page02.md)
