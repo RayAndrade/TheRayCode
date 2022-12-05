3b8afe02cfa4a860a585fe7d046a197222588e8f
Ray Andrade <TheRayCode@RayAndrade.com>
2022-12-05 09:49:49 -0800

    *

---
+++ CPP/Creational/Factory/FY1/SantasBag.h
@@ -0,0 +1,16 @@
+//
+// Created by ray on 12/5/22.
+//
+
+#ifndef FACTORY_SANTASBAG_H
+#define FACTORY_SANTASBAG_H
+
+#include "Plane.h"
+#include "Bike.h"
+#include "Car.h"
+
+class SantasBag{
+
+};
+
+#endif //FACTORY_SANTASBAG_H
---
+++ CPP/Creational/Factory/FY1/ToyFactory.h
@@ -0,0 +1,41 @@
+//
+// Created by ray on 12/5/22.
+//
+
+#ifndef FACTORY_TOYFACTORY_H
+#define FACTORY_TOYFACTORY_H
+
+#include "SantasBag.h"
+
+class ToyFactory{
+public:
+    static Toy * createToy(int type){
+        Toy *toy = NULL;
+
+        switch(type){
+            case 1:{
+                toy = new Car;
+                break;
+            }
+            case 2:{
+                toy = new Bike;
+                break;
+            }
+            case 3:{
+                toy = new Plane;
+                break;
+            }
+            default:{
+                std::cout << "Invalid toy type please re-enter type" << std::endl;
+                return NULL;
+            }
+        }
+        toy->prepareParts();
+        toy->combineParts();
+        toy->assembleParts();
+        toy->applyLabel();
+        return toy;
+    }
+};
+
+#endif //FACTORY_TOYFACTORY_H
