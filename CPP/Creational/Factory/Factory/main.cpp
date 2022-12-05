4c07138b5b7bd4dfdddb39340ead0ad64dcb3a51
Ray Andrade <TheRayCode@RayAndrade.com>
2022-12-05 09:38:24 -0800

    *

--- CPP/Creational/Factory/Factory/main.cpp
+++ CPP/Creational/Factory/Factory/main.cpp
@@ -1,27 +1,6 @@
 #include <iostream>
-//using namespace std;
-
-// https://www.youtube.com/watch?v=XyNWEWUSa5E
-// Factory Design Pattern in C++
-// CppNuts
-
-#include "ToyFactory.h"
 
 int main() {
-// client code starts
-    int type;
-    while(1){
-        std::cout << "Enter type of Zero for exit" << std::endl;
-        std::cin >> type;
-        if(!type)
-            break;
-        Toy *v = ToyFactory::createToy(type);
-        if(v){
-            v->showProduct();
-            delete v;
-        }
-    }
-    std::cout << "Exit...";
-//Client code ends
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
