cc29c99a00039cedaf8afb901a8b329c0b9732c4
Ray Andrade <TheRayCode@RayAndrade.com>
2022-12-05 10:15:50 -0800

    *

---
+++ CPP/Creational/AbstractFactory/AF1/script/page18.md
@@ -0,0 +1,5 @@
+[back](./page17.md)
+
+page18
+
+[page 19](./page19.md)
---
+++ CPP/Creational/AbstractFactory/Show/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by ray on 12/5/22.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "The Ray Code is AWESOME!!!\n";
+}
