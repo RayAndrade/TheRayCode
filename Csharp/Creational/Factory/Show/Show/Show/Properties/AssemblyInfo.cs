ff1f6e63fd434e1b626a9738eb7375d91f6b5d08
Ray Andrade <TheRayCode@RayAndrade.com>
2023-05-03 15:54:22 -0700

    *

--- Csharp/Creational/Factory/Show/Show/Show/Properties/AssemblyInfo.cs
+++ Csharp/Creational/Factory/Show/Show/Show/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Factory")]
+[assembly: AssemblyTitle("Show")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Factory")]
+[assembly: AssemblyProduct("Show")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9F2E62A5-EC4E-452E-A299-7986AE7D21E9")]
+[assembly: Guid("4B869B35-B173-49BF-B0DF-1603017D284F")]
 
 // Version information for an assembly consists of the following four values:
 //
