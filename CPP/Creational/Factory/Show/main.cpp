843cbce3d1a56ec26a3cde2722db0b288cf1286e
Ray Andrade <TheRayCode@RayAndrade.com>
2023-05-15 12:13:06 -0700

    *

--- CPP/Creational/Factory/Show/main.cpp
+++ CPP/Creational/Factory/Show/main.cpp
@@ -1,19 +1,6 @@
-#include "ShapeFactory.h"
-#include <memory>
-
-
+#include <iostream>
 
 int main() {
-    ShapeFactory factory;
-
-    std::unique_ptr<Shape> shape1(factory.getShape("CIRCLE"));
-     if (shape1) shape1->draw();
-
-    std::unique_ptr<Shape> shape2(factory.getShape("SQUARE"));
-    if (shape2) shape2->draw();
-
-    std::unique_ptr<Shape> shape3(factory.getShape("RECTANGLE"));
-    if (shape3) shape3->draw();
-
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
