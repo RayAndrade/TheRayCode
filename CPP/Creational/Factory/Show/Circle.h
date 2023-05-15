7069577b76cd441e3576174c6986f67e15d58a0e
Ray Andrade <TheRayCode@RayAndrade.com>
2023-05-15 14:05:42 -0700

    *

---
+++ CPP/Creational/Factory/Show/Circle.h
@@ -0,0 +1,17 @@
+//
+// Created by ray on 5/15/23.
+//
+
+#ifndef SHOW_CIRCLE_H
+#define SHOW_CIRCLE_H
+#include "Shape.h"
+#include <iostream>
+
+class Circle : public Shape{
+public:
+    void draw() override {
+        std::cout << "Drawing Circle." << std::endl;
+    }
+};
+
+#endif //SHOW_CIRCLE_H
