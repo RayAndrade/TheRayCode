899dae616dd444c4938301c54a5eb6afd99c9d81
Ray Andrade <TheRayCode@RayAndrade.com>
2023-05-18 11:18:07 -0700

    *

--- CPP/Creational/AbstractFactory/Show/Demo.cpp
+++ CPP/Creational/AbstractFactory/Show/Demo.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-#include "UIFactory.h"
-#include "WindowsUIFactory.h"
-#include "MacUIFactory.h"
-
 
 int main() {
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
