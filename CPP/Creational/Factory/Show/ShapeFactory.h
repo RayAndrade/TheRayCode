a878c6ce1d27a0d045188c419e71949084b95905
Ray Andrade <TheRayCode@RayAndrade.com>
2023-05-22 13:07:50 -0700

    *

---
+++ CPP/Creational/Factory/Show/ShapeFactory.h
@@ -0,0 +1,28 @@
+//
+// Created by ray on 5/22/23.
+//
+
+#ifndef SHOW_SHAPEFACTORY_H
+#define SHOW_SHAPEFACTORY_H
+
+#include "Circle.h"
+#include "Square.h"
+#include "Rectangle.h"
+#include <string>
+
+class ShapeFactory {
+public:
+    Shape* getShape(const std::string& shapeType) {
+        if (shapeType == "CIRCLE") {
+            return new Circle();
+        } else if (shapeType == "SQUARE") {
+            return new Square();
+        } else if (shapeType == "RECTANGLE") {
+            return new Rectangle();
+        }
+        return nullptr;
+    }
+};
+
+
+#endif //SHOW_SHAPEFACTORY_H
---
+++ CPP/Creational/Factory/Show/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include "ShapeFactory.h"
+#include <memory>
+
+
+int main() {
+    ShapeFactory factory;
+
+    std::unique_ptr<Shape> shape1(factory.getShape("CIRCLE"));
+    if (shape1) shape1->draw();
+
+    std::unique_ptr<Shape> shape2(factory.getShape("SQUARE"));
+    if (shape2) shape2->draw();
+
+    std::unique_ptr<Shape> shape3(factory.getShape("RECTANGLE"));
+    if (shape3) shape3->draw();
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
