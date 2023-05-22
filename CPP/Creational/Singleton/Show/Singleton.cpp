148bbc0da8c0a9dfc7fe62eb69659c433c4414d0
Ray Andrade <TheRayCode@RayAndrade.com>
2023-05-22 13:51:48 -0700

    *

---
+++ CPP/Creational/Singleton/Show/Singleton.cpp
@@ -0,0 +1,14 @@
+//
+// Created by ray on 5/22/23.
+//
+#include "Singleton.h"
+
+Singleton* Singleton::instance = 0;
+
+Singleton* Singleton::getInstance() {
+    if (instance == 0) {
+        instance = new Singleton();
+    }
+
+    return instance;
+}
