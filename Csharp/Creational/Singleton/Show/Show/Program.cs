aace64a87e55539420793e0b546b1628786c221c
Ray Andrade <TheRayCode@RayAndrade.com>
2023-05-24 13:11:33 -0700

    *

---
+++ Csharp/Creational/Singleton/Show/Show/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Show
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // The client code.
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+}
