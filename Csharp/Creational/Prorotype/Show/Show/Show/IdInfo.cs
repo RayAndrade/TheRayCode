dc4a10cadac5ae4a956e98c19e6e7439d0444400
Ray Andrade <TheRayCode@RayAndrade.com>
2023-05-24 13:34:02 -0700

    *

---
+++ Csharp/Creational/Prorotype/Show/Show/Show/IdInfo.cs
@@ -0,0 +1,12 @@
+namespace Show
+{
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+}
