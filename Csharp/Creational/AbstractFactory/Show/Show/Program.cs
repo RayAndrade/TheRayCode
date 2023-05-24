2ee84168631214bc1f2076beb00882ab0e0f0d57
Ray Andrade <TheRayCode@RayAndrade.com>
2023-05-24 12:44:48 -0700

    *

--- Csharp/Creational/AbstractFactory/Show/Show/Program.cs
+++ Csharp/Creational/AbstractFactory/Show/Show/Program.cs
@@ -6,6 +6,15 @@ namespace Show
     {
         public static void Main(string[] args)
         {
+            ShapeFactory shapeFactory = new ShapeFactory();
+            ColorFactory colorFactory = new ColorFactory();
+            
+            Circle circle = shapeFactory.GetData((int)ShapeType.Circle).DataItem as Circle;
+            circle.GetInfo();
+            
+            Red red = colorFactory.GetData((int)ColorType.Red).DataItem as Red;
+            red.GetInfo();
+            
             Console.WriteLine("The Ray Code is AWESOME!!");
         }
     }
