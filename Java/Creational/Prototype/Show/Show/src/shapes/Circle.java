0708d28e2536c7906e45f773c3317e2d0611dd98
Ray Andrade <TheRayCode@RayAndrade.com>
2023-05-25 17:31:21 -0700

    *

---
+++ Java/Creational/Prototype/Show/Show/src/shapes/Circle.java
@@ -0,0 +1,19 @@
+package shapes;
+
+public class Circle extends Shape {
+
+    public int radius;
+
+    public Circle() {
+    }
+    public Circle(Circle target) {
+        super(target);
+        if (target != null) {
+            this.radius = target.radius;
+        }
+    }
+    @Override
+    public Shape clone() {
+        return new Circle(this);
+    }
+}
