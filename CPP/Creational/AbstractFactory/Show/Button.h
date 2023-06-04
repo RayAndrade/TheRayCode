3f589719365af019d3a331d989ca01ab8b8efcc9
Ray Andrade <TheRayCode@RayAndrade.com>
2023-06-04 00:20:30 -0700

    *

---
+++ CPP/Creational/AbstractFactory/Show/Button.h
@@ -0,0 +1,13 @@
+//
+// Created by ray on 6/3/23.
+//
+
+#ifndef SHOW_BUTTON_H
+#define SHOW_BUTTON_H
+
+class Button {
+public:
+    virtual void click() = 0;
+};
+
+#endif //SHOW_BUTTON_H
---
+++ CPP/Creational/AbstractFactory/Show/WindowsButton.h
@@ -0,0 +1,18 @@
+//
+// Created by ray on 6/3/23.
+//
+
+#ifndef SHOW_WINDOWSBUTTON_H
+#define SHOW_WINDOWSBUTTON_H
+
+#include <iostream>
+#include "Button.h"
+
+class WindowsButton : public Button{
+public:
+    void click() override {
+        std::cout << "WindowsButton scroll." << std::endl;
+    }
+};
+
+#endif //SHOW_WINDOWSBUTTON_H
