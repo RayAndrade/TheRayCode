9dc644e3f856cbc13b95f5229c33ce6f4c2f7cdd
Ray Andrade <TheRayCode@RayAndrade.com>
2023-06-10 13:26:48 -0700

    *

--- Shows/ShowJava/Show/Code/src/TheRayCode/Factory/Bird.java
+++
@@ -1,8 +0,0 @@
-package TheRayCode.Factory;
-
-public class Bird implements Pet  {
-    @Override
-    public String getType() {
-        return "I am a bird.";
-    }
-}
--- Shows/ShowJava/Show/Code/src/TheRayCode/Factory/Dog.java
+++
@@ -1,8 +0,0 @@
-package TheRayCode.Factory;
-
-public class Dog implements Pet {
-    @Override
-    public String getType() {
-        return "I am a dog.";
-    }
-}
