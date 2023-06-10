dd677d932ae9f3af2298bcebb7b09294d3d1cfb6
Ray Andrade <TheRayCode@RayAndrade.com>
2023-06-10 12:33:18 -0700

    *

--- PHP/Creational/Factory/Show/index.php
+++ PHP/Creational/Factory/Show/index.php
@@ -1,17 +1,4 @@
 <?php
 namespace TheRayCode\Factory;
-include('Vehicle.php');
-include('Car.php');
-include('Motorcycle.php');
-include('Bicycle.php');
-include('VehicleFactory.php');
 
-$vehicleFactory = new VehicleFactory();
-$car = $vehicleFactory->createVehicle('car');
-$motorcycle = $vehicleFactory->createVehicle('motorcycle');
-$bicycle = $vehicleFactory->createVehicle('bicycle');
-
-$car->drive();          // Output: Driving a car...
-$motorcycle->drive();   // Output: Riding a motorcycle...
-$bicycle->drive();      // Output: Riding a bicycle...
 echo "The Ray Code is AWESOME!!!";
