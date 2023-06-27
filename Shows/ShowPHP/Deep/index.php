2b02a4c904fa4da3c817ab88717dcd77580e2f00
Ray Andrade <TheRayCode@RayAndrade.com>
2023-06-27 14:04:13 -0700

    *

--- Shows/ShowPHP/Deep/index.php
+++ Shows/ShowPHP/Deep/index.php
@@ -1,15 +1,4 @@
 <?php
 namespace TheRayCode\Prototype;
 
-require_once 'Car.php';
-
-$bmwModel = new Model("BMW");
-$car1 = new Car($bmwModel);
-$audiModel = new Model("Audi");
-$car2 = clone $car1;
-$car2->model = $audiModel;
-
-echo "Car 1 Model: " . $car1->getModel() . "<br/>"; // Outputs: "Car 1 Model: BMW"
-echo "Car 2 Model: " . $car2->getModel() . "<br/>"; // Outputs: "Car 2 Model: Audi"
-
 echo "The Ray Code is AWESOME!!!";
