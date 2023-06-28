ad15ec2c02552054c12e859a2440d3b5cdf61f1b
Ray Andrade <TheRayCode@RayAndrade.com>
2023-06-28 11:41:56 -0700

    *

--- Shows/ShowPHP/Shallow/index.php
+++ Shows/ShowPHP/Shallow/index.php
@@ -1,5 +1,6 @@
 <?php
 namespace TheRayCode\Prototype;
+
 require_once 'Car.php';
 
 $car1 = new Car("BMW");
@@ -9,4 +10,3 @@ $car2->model = "Audi";
 
 echo "Car 1 Model: " . $car1->getModel() . "<br/>"; // Outputs: "Car 1 Model: BMW"
 echo "Car 2 Model: " . $car2->getModel() . "<br/>"; // Outputs: "Car 2 Model: Audi"
-//echo "The Ray Code is AWESOME!!!";
