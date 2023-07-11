c8415b5055076ba3da5de37d0ccf046524b162c5
Ray Andrade <TheRayCode@RayAndrade.com>
2023-07-11 11:42:04 -0700

    *

--- Shows/current/csharp/Builder/Builder/Program.cs
+++ Shows/current/csharp/Builder/Builder/Program.cs
@@ -1,18 +1,12 @@
-﻿namespace Builder
+﻿using System;
+
+namespace Builder
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            DirectorIgor directorIgor = new DirectorIgor();
-            MonsterBuilderDrFrank builderDrFrank = new MonsterBuilderDrFrank();
-        
-            // Director uses the builder to construct the object
-            directorIgor.Construct(builderDrFrank);
-        
-            Monster monster = builderDrFrank.GetMonster();
-        
-            monster.Describe();
+            Console.WriteLine("TheRayCode is AWESOME!!!");
         }
     }
 }
