30ad4832ea06439e1e6daca4eda9dfca518fb05b
Ray Andrade <TheRayCode@RayAndrade.com>
2023-07-14 23:26:09 -0700

    *

--- Shows/current/java/take2/Show/src/Program.java
+++ Shows/current/java/take2/Show/src/Program.java
@@ -1,5 +1,11 @@
 public class Program {
     public static void main(String[] args) {
-        System.out.println("TheRayCode is AWESOME!!!");
+        DessertFactory dessertFactory = new DessertFactory();
+
+        Dessert dessert1 = dessertFactory.getDessert("ICECREAM");
+        dessert1.prepare();
+
+        Dessert dessert2 = dessertFactory.getDessert("CAKE");
+        dessert2.prepare();
      }
 }
