1cbfa3b3a74114e00e9fa3dd0663e2fac66ebb0a
Ray Andrade <TheRayCode@RayAndrade.com>
2023-08-07 09:10:33 -0700

    *

--- CPP/Creational/Singleton/Show/main.cpp
+++ CPP/Creational/Singleton/Show/main.cpp
@@ -1,40 +1,7 @@
 #include <iostream>
 
-#include "OnlyOne.h"
-
-#include "Ordinary.h"
-
 int main() {
-    // Wrong: cannot directly instantiate an object.
-    // OnlyOne obj;
-
-    // Correct: getting the unique instance.
-    OnlyOne& singletonObject = OnlyOne::getInstance();
-
-    // Use the singleton object.
-    singletonObject.someMethod();
-
-    Ordinary firstOrdinaryObject;
-
-    firstOrdinaryObject.printAddress();
-
-    Ordinary secondOrdinaryObject;
-
-    // Use the objects.
-    firstOrdinaryObject.someMethod();
-    firstOrdinaryObject.printAddress();
-    secondOrdinaryObject.someMethod();
-    secondOrdinaryObject.printAddress();
-
-    secondOrdinaryObject.printAddress();
+    std::cout << "Hello from OnlyOne!" << std::endl;
 
     return 0;
-}
-
-void OnlyOne::someMethod() {
-    std::cout << "Hello from OnlyOne!" , this->printAddress();
-}
-
-void Ordinary::someMethod() {
-    std::cout << "Ordinary says Hello!" , this->printAddress();
 }
