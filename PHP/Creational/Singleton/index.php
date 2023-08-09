bed7947fd212b336e2ad99b0a506282452de6eb6
Ray Andrade <TheRayCode@RayAndrade.com>
2023-08-09 11:03:48 -0700

    *

--- PHP/Creational/Singleton/index.php
+++ PHP/Creational/Singleton/index.php
@@ -9,8 +9,12 @@ $instance1 = Singleton::getInstance();
 $instance2 = Singleton::getInstance();
 
 if ($instance1 === $instance2) {
-    echo "Both instances are the same.\n";
-    echo $instance1->someMethod(); // Output: I'm a singleton!
+    echo "Both instances are the same.\n<br/>";
+    echo "instance1:";
+    echo $instance1->someMethod() . "<br/>"; // Output: I'm a singleton!
+    echo "instance2";
+    echo $instance2->someMethod() . "<br/>"; // Output: I'm a singleton!
+
 }
 ?>
 
