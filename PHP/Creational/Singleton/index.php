acbcd455fa928802da485c17cb6f5347ea675d4a
Ray Andrade <TheRayCode@RayAndrade.com>
2023-08-09 11:42:22 -0700

    *

--- PHP/Creational/Singleton/index.php
+++ PHP/Creational/Singleton/index.php
@@ -1,20 +1,5 @@
 <?php
 
 namespace TheRayCode\Singleton;
-// index.php
 
-include_once 'Singleton.php';
-
-$instance1 = Singleton::getInstance();
-$instance2 = Singleton::getInstance();
-
-if ($instance1 === $instance2) {
-    echo "Both instances are the same.\n<br/>";
-    echo "instance1:";
-    echo $instance1->someMethod() . "<br/>"; // Output: I'm a singleton!
-    echo "instance2";
-    echo $instance2->someMethod() . "<br/>"; // Output: I'm a singleton!
-
-}
-?>
-
+echo "The Ray Code is AWESOME!!!";
