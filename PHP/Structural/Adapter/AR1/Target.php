fea5a4da0ff9d1c680b0cdd278da8cf139e3aa85
Ray Andrade <TheRayCode@RayAndrade.com>
2023-08-28 11:34:47 -0700

    *

--- PHP/Structural/Adapter/AR1/Target.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Target
-{
-    public function request(): string
-    {
-        return "Target: The default target's behavior.";
-    }
-}
--- PHP/Structural/Adapter/AR2/Adaptee.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Adaptee
-{
-    public function specificRequest(): string
-    {
-        return ".eetpadA eht fo roivaheb laicepS";
-    }
-}
