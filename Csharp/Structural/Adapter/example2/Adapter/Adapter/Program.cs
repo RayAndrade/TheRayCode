dbf06b501407decc3706d97d2f601f258147a307
Ray Andrade <TheRayCode@RayAndrade.com>
2023-08-29 13:53:58 -0700

    *

--- Csharp/Structural/Adapter/example2/Adapter/Adapter/Program.cs
+++ Csharp/Structural/Adapter/example2/Adapter/Adapter/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace Adapter
+﻿using System;
+using System.Collections.Generic;
+
+namespace Adapter
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            IClientAdapter iClientAdapter = new ClientAdapter();
+            Client objClient = new Client(iClientAdapter);
+            var listOfString = objClient.GetListOfClientItem();
+            foreach (var item in listOfString)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+            Console.WriteLine("The Ray Code is AWESOME!!!");
         }
     }
 }
