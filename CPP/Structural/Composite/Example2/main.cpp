88a8066fd736b4e6870aa2a7504c4e5a74f31ca0
Ray Andrade <TheRayCode@RayAndrade.com>
2023-09-22 20:05:43 -0700

    *

---
+++ CPP/Structural/Composite/Example2/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ray on 9/22/23.
+//
+#include <iostream>
+
+int main() {
+    std::cout << "Client: I don't need to check the components classes even when managing the tree:\n";
+
+    return 0;
+}
