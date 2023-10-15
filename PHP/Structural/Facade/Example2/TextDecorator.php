ea43ba4e609dcc39c285c0d0f54e2255c27f5cf0
Ray Andrade <TheRayCode@RayAndrade.com>
2023-10-14 20:37:44 -0700

    *

--- PHP/Structural/Facade/Example2/TextDecorator.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-interface TextDecorator
-{
-    public function decorate($text);
-}
--- PHP/Structural/Facade/Example2/index.php
+++ PHP/Structural/Facade/Example2/index.php
@@ -1,21 +1,4 @@
 <?php
-require 'Facade.php';
-require 'TextDecorator.php';
-require 'BoldDecorator.php';
-require 'ItalicDecorator.php';
-require 'UnderlineDecorator.php';
 
-// Create some text
-$text = "Hello, World!";
-
-// Create decorators
-$boldDecorator = new BoldDecorator();
-$italicDecorator = new ItalicDecorator();
-$underlineDecorator = new UnderlineDecorator();
-
-// Use the Facade to apply decorators
-$formattedText = Facade::decorateText($text, [$boldDecorator, $italicDecorator, $underlineDecorator]);
-
-// Output the formatted text
-echo $formattedText;
+echo "TheRayCode is AWESOME!!!";
 
