d22b0fc9e3f47d67d095a67ca3e93497616052ec
Ray Andrade <TheRayCode@RayAndrade.com>
2023-10-23 13:32:16 -0700

    *

---
+++ PHP/show/Stage/Clown.php
@@ -0,0 +1,18 @@
+<?php
+
+class Clown
+{
+    public $name;
+    public $chatchPrase;
+
+    public function __construct($name, Phrase $phrase) {
+        $this->name = $name;
+        $this->chatchPrase = $phrase;
+    }
+
+    public function deepClone() {
+        $cloned = clone $this;
+        $cloned->chatchPrase = clone $this->chatchPrase;  // clone inner object
+        return $cloned;
+    }
+}
---
+++ PHP/show/Stage/Phrase.php
@@ -0,0 +1,10 @@
+<?php
+
+class Phrase
+{
+    public $phrase;
+
+    public function __construct($phrase) {
+        $this->phrase = $phrase;
+    }
+}
