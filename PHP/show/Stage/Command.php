c260c50582ed67595126e3b8590b884ad1ce7ee0
Ray Andrade <TheRayCode@RayAndrade.com>
2023-10-28 12:04:48 -0700

    *

--- PHP/show/Stage/Command.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-interface Command
-{
-    public function execute(): void;
-}
--- PHP/show/Stage/index.php
+++ PHP/show/Stage/index.php
@@ -1,6 +1,2 @@
 <?php
-include_once ('Command.php');
-include_once ('SimpleCommand.php');
-include_once ('ComplexCommand.php');
-include_once ('Receiver.php');
-include_once ('Invoker.php');
+echo "The Ray Code is AWESOME!!!";
