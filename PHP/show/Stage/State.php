89d88eddd0b5a45884da99fc926f69c79230d194
Ray Andrade <TheRayCode@RayAndrade.com>
2023-11-01 17:52:43 -0700

    *

---
+++ PHP/show/Stage/State.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class State
+{
+    protected $context;
+
+    public function setContext(Context $context)
+    {
+        $this->context = $context;
+    }
+
+    abstract public function handle1(): void;
+
+    abstract public function handle2(): void;
+
+}
