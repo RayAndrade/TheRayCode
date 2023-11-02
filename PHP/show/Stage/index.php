d05dbf2eb29a149b91c35d079f92bad093ffeff1
Ray Andrade <TheRayCode@RayAndrade.com>
2023-11-02 12:24:01 -0700

    *

--- PHP/show/Stage/index.php
+++ PHP/show/Stage/index.php
@@ -1,2 +1,17 @@
 <?php
-echo "The Ray Code is AWESOME!!!";
+include_once ('AbstractClass.php');
+include_once('Class1.php');
+include_once('Class2.php');
+
+function clientCode(AbstractClass $class)
+{
+    // ...
+    $class->templateMethod();
+    // ...
+}
+echo "Same client code can work with different subclasses:<br/>";
+clientCode(new Class1);
+echo "\n";
+
+echo "Same client code can work with different subclasses:<br/>";
+clientCode(new Class2);
--- PHP/show/script/page18.md
+++
@@ -1,6 +0,0 @@
-[home](./page01.md) | [back](./page17.md) | [next](./page19.md)
-
-page 18
-
-
-[page 19](./page19.md)
