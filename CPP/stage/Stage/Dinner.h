1ad6c572541b1466b737c18a10d31cafd79046e6
Ray Andrade <TheRayCode@RayAndrade.com>
2023-11-03 20:47:34 -0700

    *

---
+++ CPP/stage/Stage/Dinner.h
@@ -0,0 +1,17 @@
+//
+// Created by ray on 11/3/23.
+//
+
+#ifndef BEGINNING_DINNER_H
+#define BEGINNING_DINNER_H
+
+#include "Meal.h"
+
+class Dinner : public Meal {
+public:
+    std::string getName() const {
+        return "Dinner";
+    }
+};
+
+#endif //BEGINNING_DINNER_H
---
+++ CPP/stage/Stage/Lunch.h
@@ -0,0 +1,17 @@
+//
+// Created by ray on 11/3/23.
+//
+
+#ifndef BEGINNING_LUNCH_H
+#define BEGINNING_LUNCH_H
+
+#include "Meal.h"
+
+class Lunch : public Meal {
+public:
+    std::string getName() const {
+        return "Lunch";
+    }
+};
+
+#endif //BEGINNING_LUNCH_H
