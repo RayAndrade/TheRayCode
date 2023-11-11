1ddaedba0c8891d057c466b18dbef3f23b0e5af3
Ray Andrade <TheRayCode@RayAndrade.com>
2023-11-10 19:27:41 -0800

    *

--- CPP/stage/Stage/main.cpp
+++ CPP/stage/Stage/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "ConcretePrototype.h"
 
 int main() {
-    std::cout << "The Ray Code is AWESOME!!!" << std::endl;
+    ConcretePrototype* prototype = new ConcretePrototype("Example");
+    ConcretePrototype* clonedPrototype = dynamic_cast<ConcretePrototype*>(prototype->clone());
+
+    std::cout << "Original object data: " << prototype->getData() << std::endl;
+    std::cout << "Cloned object data: " << clonedPrototype->getData() << std::endl;
+
+    delete prototype;
+    delete clonedPrototype;
     return 0;
 }
