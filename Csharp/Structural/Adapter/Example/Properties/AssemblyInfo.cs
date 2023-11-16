eb7bf68e852a101179295d9df51060be576d806c
Ray Andrade <TheRayCode@RayAndrade.com>
2023-11-15 19:42:32 -0800

    *

--- Csharp/Structural/Adapter/Example/Properties/AssemblyInfo.cs
+++ Csharp/Structural/Adapter/Example/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Adapter")]
+[assembly: AssemblyTitle("Example3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Adapter")]
+[assembly: AssemblyProduct("Example3")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("917A932B-47E6-4CE7-AEF1-CED2358852B8")]
+[assembly: Guid("302707B9-FFA9-47CC-AD4C-9086D245F38C")]
 
 // Version information for an assembly consists of the following four values:
 //
