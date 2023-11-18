2869d10becf59c513bb09fb9f672a83e5ed02641
Ray Andrade <TheRayCode@RayAndrade.com>
2023-11-17 19:30:32 -0800

    *

--- Csharp/Structural/Bridge/Example3/Properties/AssemblyInfo.cs
+++ Csharp/Structural/Bridge/Example3/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Show")]
+[assembly: AssemblyTitle("Example3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Show")]
+[assembly: AssemblyProduct("Example3")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("AC442AE4-F38D-4BAE-A86A-CCD4DF2C5810")]
+[assembly: Guid("302707B9-FFA9-47CC-AD4C-9086D245F38C")]
 
 // Version information for an assembly consists of the following four values:
 //
