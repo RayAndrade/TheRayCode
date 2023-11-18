a9f34aa3a79703158ed7f7351d79d33338ba978f
Ray Andrade <TheRayCode@RayAndrade.com>
2023-11-17 21:38:46 -0800

    *

---
+++ Csharp/Structural/Composite/Example/Show/Show/MenuItem.cs
@@ -0,0 +1,22 @@
+namespace Show
+{
+    public class MenuItem: MenuComponent
+    {
+        private readonly string _name;
+        private readonly string _description;
+        private readonly double _price;
+
+        public MenuItem(string name, string description, double price)
+        {
+            _name = name;
+            _description = description;
+            _price = price;
+        }
+
+        public override void Print()
+        {
+            System.Console.WriteLine($"{_name}: {_description} - ${_price}");
+        }
+
+    }
+}
