edc04129a95c0f6c9cd95fd6864261f29db3eda4
Ray Andrade <TheRayCode@RayAndrade.com>
2023-12-15 12:58:23 -0800

    *

--- Shows/csharp/Show/Show/Program.cs
+++ Shows/csharp/Show/Show/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Show
+﻿using System;
+
+namespace Show
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("The Ray Code is AWESOME!!!");
         }
     }
 }
