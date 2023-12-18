9a1c0ca4f91c825ceddab638b01c1ec90ab900b2
Ray Andrade <TheRayCode@RayAndrade.com>
2023-12-18 12:38:13 -0800

    *

--- Shows/java/Example/Show/src/Main.java
+++ Shows/java/Example/Show/src/Main.java
@@ -2,4 +2,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("The Ray Code is AWESOME!!!");
     }
+
+    static class ConcreteRemote extends RemoteControl {
+        public ConcreteRemote(TV tv) {
+            super(tv);
+        }
+
+        @Override
+        public void turnOn() {
+            tv.on();
+        }
+
+        @Override
+        public void turnOff() {
+            tv.off();
+        }
+
+        @Override
+        public void setChannel(int channel) {
+            tv.tuneChannel(channel);
+        }
+    }
 }
