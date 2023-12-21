c1fdc02c2a627498eb0b8c2daab1d228ad034bf9
Ray Andrade <TheRayCode@RayAndrade.com>
2023-12-20 16:18:43 -0800

    *

---
+++ Csharp/Behavioral/Mediator/Example3/Show/Show/AbstractExpression.cs
@@ -0,0 +1,7 @@
+namespace Show
+{
+    public abstract class AbstractExpression
+    {
+        public abstract string Interpret(EmojiContext context);
+    }
+}
--- Csharp/Behavioral/Mediator/Example3/Show/Show/Program.cs
+++ Csharp/Behavioral/Mediator/Example3/Show/Show/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace Show
+﻿using System;
+namespace Show
 {
-    internal class Program
+    class Program
     {
         public static void Main(string[] args)
         {
+             var context = new EmojiContext { EmojiText = "Today I feel :)" };
+             var interpreter = new ExpressionInterpreter();
+            
+             var result = interpreter.InterpretExpressions(context);
+             Console.WriteLine($"Interpretation: {result}");
         }
     }
 }
