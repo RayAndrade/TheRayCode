c51f0f4b9c4f9292303bf84977fe6c02bbf8dcf3
Ray Andrade <TheRayCode@RayAndrade.com>
2023-12-25 14:53:40 -0800

    *

---
+++ PHP/Structural/Composite/Example/FileComponent.php
@@ -0,0 +1,10 @@
+<?php
+
+
+// The component interface declares common operations for both simple and
+// complex objects of a composition.
+interface FileComponent
+{
+    public function getSize();
+    public function getName();
+}
---
+++ PHP/Structural/Composite/Example/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'FileLeaf.php';
+require_once 'DirectoryComposite.php';
+
+// Create files
+$file1 = new FileLeaf("File1.txt", 210);
+$file2 = new FileLeaf("File2.txt", 310);
+
+// Create a directory and add files
+$directory = new DirectoryComposite("Directory");
+$directory->add($file1);
+$directory->add($file2);
+
+// Create a subdirectory and add it to the directory
+$subdirectory = new DirectoryComposite("Subdirectory");
+$subdirectory->add(new FileLeaf("SubFile1.txt", 110));
+$directory->add($subdirectory);
+
+
+// Display the size of the directory
+echo "Total Size of '" . $directory->getName() . "': " . $directory->getSize() . " bytes";
