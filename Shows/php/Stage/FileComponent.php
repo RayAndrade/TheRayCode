0cf658d843033285c9fa3f56f6761829152964e4
Ray Andrade <TheRayCode@RayAndrade.com>
2023-12-25 06:40:17 -0800

    *

--- Shows/php/Stage/FileComponent.php
+++ Shows/php/Stage/FileComponent.php
@@ -1,7 +1,5 @@
 <?php
 
-// The component interface declares common operations for both simple and
-// complex objects of a composition.
 interface FileComponent
 {
     public function getSize();
--- Shows/php/Stage/FileLeaf.php
+++ Shows/php/Stage/FileLeaf.php
@@ -1,9 +1,6 @@
 <?php
 require_once 'FileComponent.php';
 
-// The Leaf class represents end objects of a composition. A leaf can't have
-// any children. Typically, Leaf objects do the actual work, while Composite
-// objects only delegate to their sub-components.
 class FileLeaf implements FileComponent
 {
     private $name;
