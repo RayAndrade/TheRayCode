91fdf01e9cda163c9547428362253392e8faede2
Ray Andrade <TheRayCode@RayAndrade.com>
2024-01-12 14:01:50 -0800

    *

--- CPP/Behavioral/ChainOfResponsibility/Example2/main.cpp
+++ CPP/Behavioral/ChainOfResponsibility/Example2/main.cpp
@@ -1,20 +1,6 @@
-#include "Child.h"
-#include "Mother.h"
-#include "Grandmother.h"
+#include <iostream>
 
 int main() {
-    auto child = std::make_shared<Child>();
-    auto mother = std::make_shared<Mother>();
-    auto grandmother = std::make_shared<Grandmother>();
-
-    child->setNext(mother);
-    mother->setNext(grandmother);
-
-    std::cout << "Sharing a feeling of loneliness:\n";
-    child->shareFeelings("loneliness");
-
-    std::cout << "\nSharing a feeling of loss:\n";
-    child->shareFeelings("loss");
-
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
--- Shows/cpp/Stage/DishWasher.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by ray on 1/12/24.
-//
-
-#ifndef BEGINNING_DISHWASHER_H
-#define BEGINNING_DISHWASHER_H
-
-#endif //BEGINNING_DISHWASHER_H
