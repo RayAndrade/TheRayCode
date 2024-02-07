69edf2311152c527c28be8927e01ff632301b409
Ray Andrade <TheRayCode@RayAndrade.com>
2024-02-07 09:40:30 -0800

    *

--- Java/Behavioral/Observer/Example/Show/src/Main.java
+++ Java/Behavioral/Observer/Example/Show/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("The Ray Code is AWESOME!!!");
+        ConcreteSubject subject = new ConcreteSubject();
+        ConcreteObserver observer1 = new ConcreteObserver("Observer 1", subject);
+        ConcreteObserver observer2 = new ConcreteObserver("Observer 2", subject);
+
+        subject.setState(10);
+        subject.setState(20);
     }
 }
