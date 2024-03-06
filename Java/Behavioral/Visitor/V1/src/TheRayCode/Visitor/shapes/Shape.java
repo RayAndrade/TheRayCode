a59edc6cab524af306715b8e779e899f8174559e
Ray Andrade <TheRayCode@RayAndrade.com>
2024-03-06 12:30:59 -0800

    *

--- Java/Behavioral/Visitor/V1/src/TheRayCode/Visitor/shapes/Shape.java
+++
@@ -1,9 +0,0 @@
-package TheRayCode.Visitor.shapes;
-
-import TheRayCode.Visitor.visitor.Visitor;
-
-public interface Shape {
-    void move(int x, int y);
-    void draw();
-    String accept(Visitor visitor);
-}
