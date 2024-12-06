ca0f5fb340df6bb1cc5a04817ec695600e721d3f
Ray Andrade <TheRayCode@RayAndrade.com>
2024-12-05 19:53:59 -0800

    yes

--- Csharp/Behavioral/ChainOfResponsibility/Example/ChainOfResponsibility/Program.cs
+++ Csharp/Behavioral/ChainOfResponsibility/Example/ChainOfResponsibility/Program.cs
@@ -7,19 +7,6 @@ namespace ChainOfResponsibility
     {
         static void Main(string[] args)
         {
-            // The other part of the client code constructs the actual chain.
-            var mouse = new MouseHandler();
-            var cat = new CatHandler();
-            var dog = new DogHandler();
-
-            mouse.SetNext(cat).SetNext(dog);
-
-            // The client should be able to send a request to any handler, not
-            // just the first one in the chain.
-            Console.WriteLine("Chain: Dog > Cat > Mouse\n");
-            Client.ClientCode(mouse);
-            Console.WriteLine();
-
             Console.WriteLine("Subchain: Dog > Cat\n");
             Client.ClientCode(cat);
         }
