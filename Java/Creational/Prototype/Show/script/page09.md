5836266c90ed57c617b3aa23353c7f772abc9599
Ray Andrade <TheRayCode@RayAndrade.com>
2024-12-17 14:23:59 -0800

    *

--- Java/Creational/Prototype/Show/script/page09.md
+++
@@ -1,7 +0,0 @@
-[home](./page01.md)
-
-[back](./page08.md)
-
-The end
-
-[goto page 1](./page01.md)
--- Java/Creational/Prototype/Show/src/Pie.java
+++
@@ -1,6 +0,0 @@
-public class Pie implements Dessert{
-    @Override
-    public void prepare() {
-        System.out.println("Preparing cherry pie...");
-    }
-}
