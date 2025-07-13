d5d9f982847dd626ec946978ebe0a539c0610b39
Ray Andrade <TheRayCode@RayAndrade.com>
2025-07-13 14:20:38 -0700

    *

--- JavaScript/Structural/Bridge/Example1/Implementor.js
+++
@@ -1,8 +0,0 @@
-// Purpose: Interface for implementation classes. Defines a method that must be implemented.
-
-export class Implementor {
-    // This method is intended to be overridden by subclasses
-    operationImpl() {
-        throw new Error("operationImpl() must be implemented by subclass.");
-    }
-}
--- JavaScript/Structural/Bridge/Example1/RefinedAbstraction.js
+++
@@ -1,9 +0,0 @@
-// RefinedAbstraction.js
-import { Abstraction } from './Abstraction.js';
-
-export class RefinedAbstraction extends Abstraction {
-    operation() {
-        console.log("RefinedAbstraction: operation() is delegating to implementor...");
-        this.implementor.operationImpl();
-    }
-}
